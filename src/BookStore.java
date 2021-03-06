f3cefc286eecc5f462984353cdeec30968164275
Hestia <ducanhvu@nhatanh.edu.vn>
2021-03-06 15:29:45 -0500

    Selection Sort ISBN

--- src/BookStore.java
+++ src/BookStore.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 public class BookStore {
 
     // Method to read from a text file into an array of Book
-    public static int inputData (String fileName, Book inBook[]) {
+    public static int inputData (String fileName, Book[] Book) {
         int i = 0;
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
-            while (in.ready() == true) {
-                inBook[i] = new Book();
-                inBook[i].setBookTitle(in.readLine());
-                inBook[i].setBookAuthor(in.readLine());
-                inBook[i].setBookPublisher(in.readLine());
-                inBook[i].setNumberISBN(Long.parseLong(in.readLine()));
-                inBook[i].setBookPrice(Double.parseDouble(in.readLine()));
-                inBook[i].setStockQuantity(Integer.parseInt(in.readLine()));
+            while (in.ready()) {
+                Book[i] = new Book();
+                Book[i].setBookTitle(in.readLine());
+                Book[i].setBookAuthor(in.readLine());
+                Book[i].setBookPublisher(in.readLine());
+                Book[i].setNumberISBN(Long.parseLong(in.readLine()));
+                Book[i].setBookPrice(Double.parseDouble(in.readLine()));
+                Book[i].setStockQuantity(Integer.parseInt(in.readLine()));
                 i++;
             }
             in.close();
@@ -26,16 +26,16 @@ public class BookStore {
     }
 
     // Print out field within an array of Book
-    public static void printOutData (Book printBooks[], int numberRecord) {
+    public static void printOutData (Book[] Book, int numberRecord) {
         for (int i = 0; i < numberRecord; i++) {
-            System.out.println(printBooks[i].getBookTitle() + "\t" + printBooks[i].getBookAuthor()
-                    + "\t" + printBooks[i].getBookPublisher() + "\t" + printBooks[i].getNumberISBN()
-                    + "\t" + printBooks[i].getBookPrice() + "\t" + printBooks[i].getStockQuantity());
+            System.out.println(Book[i].getBookTitle() + "\t" + Book[i].getBookAuthor()
+                    + "\t" + Book[i].getBookPublisher() + "\t" + Book[i].getNumberISBN()
+                    + "\t" + Book[i].getBookPrice() + "\t" + Book[i].getStockQuantity());
         }  // end printOutData
     }
 
     // Method to write a binary file from an array of Book
-    public static void writeNewBinFile(String fileName, Book Book[], int numberRecord) throws IOException{
+    public static void writeNewBinFile(String fileName, Book[] Book, int numberRecord) throws IOException{
         RandomAccessFile randomAccessFile = new RandomAccessFile(fileName, "rw");
         for (int i=0; i < numberRecord; i++) {
             Book[i].writeRecord(randomAccessFile, i);
@@ -43,7 +43,7 @@ public class BookStore {
         randomAccessFile.close();
     } // end writeNewBinFile
 
-    public static int readNewBinFile(String fileName, Book Book[]) throws IOException{
+    public static int readNewBinFile(String fileName, Book[] Book) throws IOException{
         RandomAccessFile randomAccessFile = new RandomAccessFile(fileName,"rw");
         System.out.println("The length is "+ randomAccessFile.length());
         int numberRecord = (int)randomAccessFile.length()/190;
@@ -60,7 +60,8 @@ public class BookStore {
         return numberRecord;
     } // end writeNewBinFile
 
-    public static void printReport (Book Book[]) {
+    // Method printReport
+    public static void printReport (Book[] Book) {
         for (int i = 0; i < Book.length; i++) {
             System.out.println();
             System.out.println("Book #" + (i + 1));
@@ -70,17 +71,33 @@ public class BookStore {
             System.out.println("ISBN: " + Book[i].getNumberISBN());
             System.out.println("Price: " + Book[i].getBookPrice());
             System.out.println("Quantity: " + Book[i].getStockQuantity());
-            System.out.println("Total Price: " + (Book[i].getBookPrice()*Book[i].getStockQuantity()));
+            System.out.println("Total Price: " + (Book[i].getBookPrice() * Book[i].getStockQuantity()));
             System.out.println();
             System.out.println("====================================");
         }
     }
 
+    // Method sortByISBN
+    public static void sortByISBN(Book[] book) {
+        int n = book.length;
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int minISBN = i;
+            for (int j = i + 1; j < n; j++)
+                if (book[j].getNumberISBN() < book[minISBN].getNumberISBN())
+                    minISBN = j;
 
-    public static void main (String[] args) throws IOException {
+            // Swap the found minimum element with the first element
+            Book temp = book[minISBN];
+            book[minISBN] = book[i];
+            book[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         System.out.println(' ');
         int records = 0;
-        String ans;
         String txtFileName, binFileName;
         Scanner s = new Scanner(System.in);
         Book[] books = new Book[12];
@@ -121,17 +138,18 @@ public class BookStore {
                     break;
                 case 2:
                     System.out.println("Please enter the Binary file name you want to read from with extension:");
-                    binFileName= s.nextLine();
+                    binFileName = s.nextLine();
                     records = readNewBinFile(binFileName, books);
                     printOutData(books, records);
                     break;
                 case 3:
                     System.out.println("Please enter the Binary file name you want to write to:");
-                    binFileName= s.nextLine();
+                    binFileName = s.nextLine();
                     writeNewBinFile(binFileName, books, records);
                     break;
                 case 4:
-
+                    sortByISBN(books);
+                    printOutData(books, records);
                     break;
                 case 5:
                     printReport(books);
@@ -150,7 +168,7 @@ public class BookStore {
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("Your selection is not available! Please rechoose.");
+                    System.out.println("Your selection is not available! Please choose again.");
                     break;
             }
         }
